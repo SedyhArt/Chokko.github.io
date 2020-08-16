efd9e38aa35484e9cf7e0c99ed03ce52482eb98b
Artem Sedykh <sedixtema@icloud.com>
2020-08-16 15:42:10 +0300

    добавлено полноэкранное меню

---
+++ js/overlay.js
@@ -0,0 +1,9 @@
+let hamburger = document.querySelector(".hamburger");
+let overlay = document.querySelector(".overlay");
+
+function toggleMenu(){
+  hamburger.classList.toggle('hamburger--active');
+  overlay.classList.toggle('overlay--active');
+}
+
+hamburger.addEventListener('click' , toggleMenu);
