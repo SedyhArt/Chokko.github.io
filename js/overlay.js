ebccb3a4b4e8dd4cda3a5850db595e2bbe8ff2c7
Artem Sedykh <sedixtema@icloud.com>
2020-08-23 17:22:38 +0300

    Исправлено оверлей меню и доабвлено слайдшоу

--- js/overlay.js
+++ js/overlay.js
@@ -1,9 +1,15 @@
 let hamburger = document.querySelector(".hamburger");
 let overlay = document.querySelector(".overlay");
 
+let links = document.querySelectorAll(".overlay__menu .menu__link");
+
+
 function toggleMenu(){
   hamburger.classList.toggle('hamburger--active');
   overlay.classList.toggle('overlay--active');
 }
 
 hamburger.addEventListener('click' , toggleMenu);
+links.forEach(function(element){
+  element.addEventListener('click', toggleMenu);
+})
