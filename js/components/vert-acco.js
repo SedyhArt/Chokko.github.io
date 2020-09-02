c46a0b82d15b2da1030ad3d5b061fef3bdbd0ea0
Artem Sedykh <sedixtema@icloud.com>
2020-09-02 22:43:25 +0300

    Красивое поылвение текста в верт. аакорд.

--- js/components/vert-acco.js
+++ js/components/vert-acco.js
@@ -1,15 +1,25 @@
 const mesureWidth = function(item) {
+  let reqItemWidth = 0;
   const screenWidth = $(window).width();
   const container = item.closest(".products-menu");
   const titlesBlocks = container.find(".products-menu__title");
   const titlesWidth = titlesBlocks.width() * titlesBlocks.length;
 
+  const textContainer =  item.find(".products-menu__container");
+  const paddingLeft = parseInt(textContainer.css("padding-left"));
+  const paddingRight = parseInt(textContainer.css("padding-right"));
+
   const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
   if (isMobile) {
-    return screenWidth - titlesWidth;
+    reqItemWidth = screenWidth - titlesWidth;
   } else {
-    return 500;
+    reqItemWidth = 500;
+  }
+
+  return {
+    container: reqItemWidth,
+    textContainer: reqItemWidth - paddingRight - paddingLeft
   }
 }
 
@@ -20,16 +30,20 @@ $(".products-menu__title").on("click", e => {
   const item = $this.closest(".products-menu__item");
   const container = $this.closest(".products-menu");
   const itemOpened = item.hasClass("active");
+  const textBlock = item.find(".products-menu__container");
   
   item.addClass("active");
+  item.find(".products-menu__content").width(mesureWidth(item).container);
+  textBlock.width(mesureWidth(item).textContainer);
+  console.log(mesureWidth(item).container);
 
   if (itemOpened) {
     closeEveryItemCotainer(container);
   } else {
     closeEveryItemCotainer(container);
-    item.find(".products-menu__content").width(mesureWidth(item));
+    item.find(".products-menu__content").width(mesureWidth(item).container);
   }
-  // openItem(item);
+  
 })
 
 $(".products-menu__close").on("click", e => {
